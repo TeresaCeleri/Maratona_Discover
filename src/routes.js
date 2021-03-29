f927293a8b7e15efb2a0958ebfd267b2451109c2
teresaceleri <teresaceleri18@gmail.com>
2021-03-29 17:47:58 -0300

    alterado_29032021

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const routes = express.Router()
 
-const basePath = __dirname + "/views"
+//basePath é o caminho base
+const views = __dirname + "/views/"
 
-routes.get('/', (request,response) => response.sendFile(basePath + "/index.html"))
-routes.get('/job', (request,response) => response.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (request,response) => response.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (request,response) => response.sendFile(basePath + "/profile.html"))
+//criar um objeto
+const profile = {
+   name: "Teresa",
+   avatar: "https://xesque.rocketseat.dev/users/avatar/profile-db27254b-0432-4f77-a157-852603420f70-1597772278861.jpg",
+   "monthly-budget": 3000,
+   "days-per-week": 5,
+   "hours-per-week": 5,
+   "vacation-per-year": 4
+}
+
+//req de request(pesgunta) e res de response(resposta) 
+//trocar senFile por render de renderizar
+//não precisa basePath e trocar "/index.html" por "index"
+routes.get('/', (req,res) => res.render(views + "index"))
+routes.get('/job', (req,res) => res.render(views + "job"))
+routes.get('/job-edit', (req,res) => res.render(views + "job-edit"))
+routes.get('/profile', (req,res) => res.render(views + "profile", { profile}))
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
+//express é uma biblioteca para criar o servidor 
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+
+//vai usar o motor do ejs, ou seja, renderizar
+//usando template engine
+server.set("view engine", 'ejs')
 
 //habilitar arquivos estaticos
 server.use(express.static("public"))
@@ -8,4 +13,4 @@ server.use(express.static("public"))
 //routes
 server.use(routes)
 
-server.listen(3000, () => console.log('rodando'))
+server.listen(3005, () => console.log('rodando'))
