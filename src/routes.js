2296bc62eba78ed2203a5e8f5d0212dc623d7544
teresaceleri <teresaceleri18@gmail.com>
2021-03-30 16:53:32 -0300

    finalizado_aula1

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,29 @@
 const express = require("express");
-const routes = express.Router()
+const routes = express.Router();
 
 //basePath é o caminho base
-const views = __dirname + "/views/"
+const views = __dirname + "/views/";
 
 //criar um objeto
 const profile = {
-   name: "Teresa",
-   avatar: "https://xesque.rocketseat.dev/users/avatar/profile-db27254b-0432-4f77-a157-852603420f70-1597772278861.jpg",
-   "monthly-budget": 3000,
-   "days-per-week": 5,
-   "hours-per-week": 5,
-   "vacation-per-year": 4
-}
+  name: "Teresa",
+  avatar:
+    "https://xesque.rocketseat.dev/users/avatar/profile-db27254b-0432-4f77-a157-852603420f70-1597772278861.jpg",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-week": 5,
+  "hours-per-day": 8,
+  "vacation-per-year": 4,
+};
 
-//req de request(pesgunta) e res de response(resposta) 
+//req de request(pesgunta) e res de response(resposta)
 //trocar senFile por render de renderizar
 //não precisa basePath e trocar "/index.html" por "index"
-routes.get('/', (req,res) => res.render(views + "index"))
-routes.get('/job', (req,res) => res.render(views + "job"))
-routes.get('/job-edit', (req,res) => res.render(views + "job-edit"))
-routes.get('/profile', (req,res) => res.render(views + "profile", { profile}))
+routes.get("/", (req, res) => res.render(views + "index"));
+routes.get("/job", (req, res) => res.render(views + "job"));
+routes.get("/job-edit", (req, res) => res.render(views + "job-edit"));
+routes.get("/profile", (req, res) =>
+  res.render(views + "profile", { profile })
+);
 
 module.exports = routes;
